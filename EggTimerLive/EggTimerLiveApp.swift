7659c5356156d4a99c229cf03dcf9f101e0c10db
nikadari <nikadariani@gmail.com>
2021-08-25 21:16:57 -0400

    Initial Commit

---
+++ EggTimerLive/EggTimerLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerLiveApp.swift
+//  EggTimerLive
+//
+//  Created by Nika Dariani on 2021-08-25.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
